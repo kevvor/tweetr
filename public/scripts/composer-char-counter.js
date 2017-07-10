3ff2663a4dbe8ec5d2392d0e9c50f95e98a92c7a
keano voelkl <keanov@gmail.com>
2017-07-10 23:47:44 +0000

    Implemented character counter. Turns red when character limit is exceeded

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+let text = $('#tweet-field');
+
+text.on("input", function(event) {
+  $(this).siblings('#count').text(140 - this.value.length)
+  if (this.value.length > 140) {
+    $(this).siblings('#count').addClass("count-red");
+  } else if (this.value.length <= 140) {
+    $(this).siblings('#count').removeClass("count-red");
+  }
+});
+
+});
+
