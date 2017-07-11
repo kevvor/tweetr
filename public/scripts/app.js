43e6d471ff5feee02a8d67f56fdb13c0e5347a9d
keano voelkl <keanov@gmail.com>
2017-07-11 23:41:39 +0000

    can create article from tweetdata

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,98 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+function createTweetElement(data) {
+  let article = $("<article>").addClass("tweet")
+  let header = $("<header>");
+  let avatar = $("<img>");
+  let fullName = $("<h2>");
+  let handle = $("<span>");
+  let tweetContent = $("<div>");
+  let tweet = $("<p>")
+  let footer = $("<footer>");
+  let buttons = $("<span>");
+  let flag = $("<i>");
+  let retweet = $("<i>");
+  let heart = $("<i>");
+
+  fullName.text(tweetData.user.name);
+  handle.text(tweetData.user.handle);
+  avatar.attr('src', tweetData.user.avatars.small);
+  header.append(avatar, fullName, handle);
+
+  tweet.addClass("tweet-content");
+  tweet.text(tweetData.content.text);
+  tweetContent.append(tweet);
+
+  flag.addClass("fa fa-flag");
+  flag.attr("aria-hidden", "true");
+
+  retweet.addClass("fa fa-retweet");
+  retweet.attr("aria-hidden", "true");
+
+  heart.addClass("fa fa-heart");
+  heart.attr("aria-hidden", "true");
+
+  buttons.append(flag, retweet, heart);
+  footer.append(buttons);
+  footer.text(tweetData.created_at);
+
+  article.append(header, tweetContent, footer);
+
+  return article;
+}
+
+function renderTweets(arrayOfTweets) {
+  for (tweets of arrayOfTweets) {
+
+  }
+}
+
+
+
+
 
--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 let tweetContainer = $('.tweet-container')
 
 tweetContainer.on("mouseenter", function (event) {
-  console.log('in tweetContainer')
   $(this).find('.buttons').removeClass("hidden");
 });
 
